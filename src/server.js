93d101ccc93e09596cc265d44c6330588b6701db
audxce <saqures9@CougarNet.UH.EDU>
2024-10-31 18:26:19 -0500

    fixed server

--- src/server.js
+++ src/server.js
@@ -8,9 +8,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
 
 // Import routes
 const loginRoutes = require("./routes/login");
